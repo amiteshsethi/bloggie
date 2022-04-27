7106363c7a35abc4849f079af11c25f6e6be3159
amiteshsethi <amiteshsethi@gmail.com>
2022-04-28 01:32:38 +0530

    adding firebase to Home Page for Dispay of Posts

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,9 +1,45 @@
-
+import {firestore} from '../firebase';
 import React from "react";
+import {useState} from "react";
 
 function CreatePost() {
-    return <div className="CreatePost">
-        This is CreatePost
+    const [title,setTitle] = useState();
+    const [subTitle,setSubTitle] = useState();
+    const [content , setContent] = useState();
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log('title',title)
+        console.log('subTitle',subTitle)
+        console.log('content',content)
+
+        firestore.collection('posts').add({
+            title,
+            subTitle,
+            content,
+            createdAt : new Date(),
+        })
+    }
+
+    return <div className="create-post">
+        <h1>Create a Post</h1>
+
+        <form onSubmit={handleSubmit}>
+            <div className="form-field">
+                <label>Title of Your Post</label>
+                <input value={title} onChange={(e) =>setTitle(e.target.value) }/>
+            </div>
+            <div className="form-field">
+                <label>Context/Sub-Title</label>
+                <input value={subTitle} onChange={(e) => setSubTitle(e.target.value) }/>
+            </div>
+            <div className="form-field">
+                <label>Content</label>
+                <textarea value={content} onChange={(e) => setContent(e.target.value)}></textarea>
+            </div>
+
+            <button className="create-post-btn">Create Post</button>
+        </form>
     </div>
 
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from 'react-router-dom'
+import { firestore } from "../firebase";
 
 function Home() {
-    return <div className="Home">
-        <h1>This is Home </h1>
-    </div>
+    const [post, setPosts] = useState([])
 
+    useEffect(() => {
+        firestore.collection('posts').get().then((snapshot) => {
+            const posts = snapshot.docs.map((docs) => {
+                return {
+                    id: docs.id,
+                    ...docs.data()
+                }
+            })
+            console.log('posts', posts)
+            setPosts(posts)
+        })
+    }, [])
+
+
+    return (
+        <div className="home">
+        <h1> Welcome to Bloggie </h1>
+
+        {post.map( (post,index) => (
+            <div className="post" key={`post-${index}`}>
+                <Link to={`/post/${post.id}`}>
+                    <h3>{post.title}</h3>
+                </Link>
+
+                <p>{post.subTitle}</p>
+            </div>
+
+        ))}
+    </div>
+    )
 }
 
 export default Home
